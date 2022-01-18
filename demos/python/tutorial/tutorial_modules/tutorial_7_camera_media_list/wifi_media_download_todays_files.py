8a76f6b6afa5f3c9b710eaff8d872a5fa7e09f69
takerfume <yoshida.taketo1995@gmail.com>
2022-01-18 16:08:25 +0000

    WIP：今日の日付に一致するデータだけ取得するコードを作成ずみ。ディレクトリの指定はまだ

--- demos/python/tutorial/tutorial_modules/tutorial_7_camera_media_list/wifi_media_download_todays_files.py
+++ demos/python/tutorial/tutorial_modules/tutorial_7_camera_media_list/wifi_media_download_todays_files.py
@@ -10,20 +10,35 @@ import requests
 
 from tutorial_modules import GOPRO_BASE_URL, get_media_list
 import glob
+import datetime
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 
 def main():
+    dt_now_jst_aware = datetime.datetime.now(
+    datetime.timezone(datetime.timedelta(hours=9))
+)
+
     # Get the media list
     media_list = get_media_list()
+    print(media_list["media"])
 
     # Find a photo. We're just taking the first one we find.
     photo: Optional[str] = []
     hero_video: Optional[str] = []
     sphere_video: Optional[str] = []
-    for media_file in [x["n"] for x in media_list["media"][0]["fs"]]:
+    for media_info in [x for x in media_list["media"][0]["fs"]]:
+        creation_unix_time = media_info["cre"]
+        creation_dt_jst_aware = datetime.datetime.fromtimestamp(float(creation_unix_time), datetime.timezone(datetime.timedelta(hours=9)))
+        print(dt_now_jst_aware.date())
+        print(creation_dt_jst_aware.date())
+        if dt_now_jst_aware.date() != creation_dt_jst_aware.date():
+            continue
+    
+        media_file = media_info["n"]
         if media_file.lower().endswith(".jpg"):
             logger.info(f"found a photo: {media_file}")
             photo.append(media_file)
@@ -39,11 +54,12 @@ def main():
 
     # ディレクトリ内のファイル一覧取得
     
-
+    file_paths_360 = glob.glob(f"{args.dir_path}/*.360")
 
     # Build the url to get the thumbnail data for the photo
     for media_list, file_ext in zip([sphere_video], [".360"]):
         for media_file in media_list:
+            print(media_file)
             logger.info(f"Downloading {media_file}")
             url = GOPRO_BASE_URL + f"/videos/DCIM/100GOPRO/{media_file}"
             logger.info(f"Sending: {url}")
@@ -59,5 +75,5 @@ def main():
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Find a media on the camera and download it to the computer.")
     parser.add_argument("--dir_path", type=str)
-    parser.parse_args()
+    args = parser.parse_args()
     main()
