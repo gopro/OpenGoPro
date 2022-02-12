427fced5aef861dbf73a4076726c2d0668a2e416
Tim Camise <tcamise@gopro.com>
2022-02-11 18:21:59 -0800

    Improve search (#130)
    * Customize search store
    * Add other files to search and clean up excerpts

---
+++ docs/assets/js/lunr/lunr-store.js
@@ -0,0 +1,68 @@
+/* lunr-store.js/Open GoPro, Version 2.0 (C) Copyright 2021 GoPro, Inc. (http://gopro.com/OpenGoPro). */
+/* This copyright was auto-generated on Sat Feb 12 02:16:19 UTC 2022 */
+
+---
+layout: none
+---
+
+{% assign searchable_pages = site.pages | where_exp: "page", "page.title and page.search != false" %}
+{%- for collection in site.collections -%}
+    {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%}
+    {%- for doc in docs -%}
+        {% assign searchable_pages = searchable_pages | push: doc %}
+    {%- endfor -%}
+{%- endfor -%}
+
+
+var store = [
+    {%- for doc in searchable_pages -%}
+      {%- assign last_section = false -%}
+      {%- if forloop.last -%}
+            {%- assign last_doc = true -%}
+      {%- endif -%}
+      {%- assign sections = doc.content | newline_to_br | strip_newlines | split: "<br />#" -%}
+      {%- for section in sections -%}
+        {%- if forloop.last -%}
+            {%- assign last_section = true -%}
+        {%- endif -%}
+
+        {%- assign tokens = section | split: "<br />" -%}
+        {%- if forloop.first -%}
+            {%- assign id = "" -%}
+            {%- assign slug = id -%}
+        {% elsif tokens.size == 1 %}
+            {%- continue -%}
+        {% else %}
+            {%- assign id = tokens[0] | remove: "#" | strip -%}
+            {%- assign slug = id | slugify -%}
+        {% endif %}
+        {
+            "title": {{ doc.title | append: ": " | append: id | jsonify }},
+            "excerpt": {{ section |
+                newline_to_br |
+                replace:"<br />", " " |
+                replace:"</p>", " " |
+                replace:"</h1>", " " |
+                replace:"</h2>", " " |
+                replace:"</h3>", " " |
+                replace:"</h4>", " " |
+                replace:"</h5>", " " |
+                replace:"</h6>", " " |
+                strip_html |
+                remove: "#" |
+                remove_first: id |
+                strip_newlines |
+                strip |
+                jsonify }},
+            "categories": [],
+            "tags": [],
+            "url": {{ doc.url | relative_url |
+                        append: "#" |
+                        append: slug |
+                        jsonify }},
+            "teaser": ''
+        }
+      {%- unless last_file and last_section -%},{%- endunless -%}
+    {%- endfor -%}
+    {%- endfor -%}
+]
