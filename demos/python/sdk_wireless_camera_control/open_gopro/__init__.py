0c2c6b0d9d99d6511b683b61678a62fe47733a6a
Tim Camise <tcamise@gopro.com>
2022-03-04 13:36:54 -0800

    squash this

--- demos/python/sdk_wireless_camera_control/open_gopro/__init__.py
+++ demos/python/sdk_wireless_camera_control/open_gopro/__init__.py
@@ -1,16 +1,19 @@
 # __init__.py/Open GoPro, Version 2.0 (C) Copyright 2021 GoPro, Inc. (http://gopro.com/OpenGoPro).
 # This copyright was auto-generated on Wed, Sep  1, 2021  5:05:51 PM
 
+# pylint: disable=wrong-import-position
+
 """All GoPro exports that the the user will want should be exported here."""
+
+import logging
 
 # Extract version set from pyproject.toml
 import importlib.metadata as importlib_metadata
 
 __version__ = importlib_metadata.version(__name__)
 
-# Set up custom logging level
 from open_gopro.util import addLoggingLevel
-import logging
+
 addLoggingLevel("TRACE", logging.DEBUG - 5)
 
 from open_gopro.gopro import GoPro
