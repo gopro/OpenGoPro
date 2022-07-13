007081a38fc39123448f8e9f838f83f0e5995a42
Tim Camise <tcamise@gopro.com>
2022-07-13 16:03:58 -0700

    Improve accordion plugin (#208)

--- docs/assets/js/accordion.js
+++ docs/assets/js/accordion.js
@@ -11,6 +11,9 @@ window.addEventListener('load', function () {
                 /* Toggle between adding and removing the "active" class,
                 to highlight the button that controls the panel */
                 this.classList.toggle('accordion-active');
+                var arrow = this.getElementsByTagName('i')[0];
+                arrow.classList.toggle('fa-chevron-down');
+                arrow.classList.toggle('fa-chevron-up');
 
                 /* Toggle between hiding and showing the active panel */
                 var panel = this.nextElementSibling;
