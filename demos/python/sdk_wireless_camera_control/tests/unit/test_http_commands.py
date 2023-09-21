acd7c3027ea607b154dc5465ccc8e9180eed50fc
Tim Camise <tcamise@gopro.com>
2023-09-21 13:23:18 -0700

    Fix Set Setting URL in the Python SDK (#402)

--- demos/python/sdk_wireless_camera_control/tests/unit/test_http_commands.py
+++ demos/python/sdk_wireless_camera_control/tests/unit/test_http_commands.py
@@ -7,6 +7,7 @@ from pathlib import Path
 
 import pytest
 
+from open_gopro import Params
 from open_gopro.gopro_base import GoProBase
 
 camera_file = "100GOPRO/XXX.mp4"
@@ -16,6 +17,12 @@ camera_file = "100GOPRO/XXX.mp4"
 async def test_get_with_no_params(mock_wifi_communicator: GoProBase):
     response = await mock_wifi_communicator.http_command.get_media_list()
     assert response.url == "gopro/media/list"
+
+
+@pytest.mark.asyncio
+async def test_set_setting(mock_wifi_communicator: GoProBase):
+    response = await mock_wifi_communicator.http_setting.fps.set(Params.FPS.FPS_100)
+    assert response.url == "gopro/camera/setting?setting=3&option=2"
 
 
 @pytest.mark.asyncio
