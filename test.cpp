155bc71da7aac724e60685967855d5a8062e6d98
Tim Camise <tcamise@gopro.com>
2024-04-10 11:36:07 -0700

    Update

--- test.cpp
+++ test.cpp
@@ -1,3 +1,2 @@
-/* test.cpp/Open GoPro, Version 2.0 (C) Copyright 2021 GoPro, Inc. (http://gopro.com/OpenGoPro). */
-/* This copyright was auto-generated on Wed Apr 10 18:16:25 UTC 2024 */
 
+// Something
