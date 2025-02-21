39ec626918cf33e70deeb7eaa4851003c1f9c7ce
Tim Camise <tcamise@gopro.com>
2025-02-21 09:28:03 -0800

    Update makefile goal

--- tools/proto_build/entrypoint.sh
+++ tools/proto_build/entrypoint.sh
@@ -3,13 +3,11 @@
 # entrypoint.sh/Open GoPro, Version 2.0 (C) Copyright 2021 GoPro, Inc. (http://gopro.com/OpenGoPro).
 # This copyright was auto-generated on Wed Mar 27 22:05:54 UTC 2024
 
-
 PROTO_SRC_DIR=/proto_in
 PROTO_PYTHON_OUT_DIR=/proto_output/python
 PROTO_KOTLIN_OUT_DIR=/proto_output/kotlin
 
-function build_python()
-{
+function build_python() {
     rm -rf $PROTO_PYTHON_OUT_DIR/* && mkdir -p $PROTO_PYTHON_OUT_DIR
     echo
     echo "Building protobuf python files and stubs from .proto source files..."
@@ -24,14 +22,11 @@ function build_python()
     rm descriptors
 }
 
-function build_kotlin()
-{
-    set -x
+function build_kotlin() {
     echo "Building kotlin protobuf files using pbandk..."
     export PATH=$PATH:/
     rm -rf $PROTO_KOTLIN_OUT_DIR/* && mkdir -p $PROTO_KOTLIN_OUT_DIR
     gosu user:user mkdir -p /home/user/temp
-    ls -la /home/user/temp
     # We can't run as root because pbank can't handle it: https://github.com/streem/pbandk/issues/73
     gosu user:user protoc --pbandk_out=kotlin_package=com.gopro.open_gopro.operations,visibility=internal:/home/user/temp -I $PROTO_SRC_DIR $PROTO_SRC_DIR/*
     mv /home/user/temp/com/gopro/open_gopro/operations/* $PROTO_KOTLIN_OUT_DIR
@@ -39,3 +34,5 @@ function build_kotlin()
 
 build_python
 build_kotlin
+
+echo "Protobuf build complete!"
