ce622f5139d5219c89c774a4157f21d02e43b421
Tim Camise <tcamise@gopro.com>
2025-02-27 13:18:44 -0800

    E2E working

--- tools/proto_build/kmp_proto_manipulator/kmp_proto_manipulator/parsers/kotlin_transformer.py
+++ tools/proto_build/kmp_proto_manipulator/kmp_proto_manipulator/parsers/kotlin_transformer.py
@@ -1,5 +1,5 @@
 from kmp_proto_manipulator.entities.config import Config
-from kmp_proto_manipulator.grammar.kotlin_proto import scope, kotlin_object, object_name
+from kmp_proto_manipulator.grammar.kotlin_proto import kotlin_object
 
 
 class KotlinTransformer:
@@ -9,10 +9,13 @@ class KotlinTransformer:
         self._transformer = kotlin_object
 
     def _manipulate_scope(self, toks) -> str:
+        print(f"REMOVE ME {toks.name}")
         if spec := self.config.root.get(toks.name):
+            print(f"using scope {spec.scope}")
             scope = spec.scope
         else:
-            scope = toks["scope"]
+            print(f"using default scope {toks.scope}")
+            scope = toks.scope
         return " ".join([scope, *toks[1:]])
 
     def transform(self, kotlin: str) -> str:
--- tools/proto_build/kmp_proto_manipulator/tests/conftest.py
+++ tools/proto_build/kmp_proto_manipulator/tests/conftest.py
@@ -3,21 +3,26 @@ from pathlib import Path
 import pytest
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def enum_as_text() -> str:
     return (Path("tests") / "vectors" / "enum.kt").read_text()
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def sealed_class_as_text() -> str:
     return (Path("tests") / "vectors" / "sealed_class.kt").read_text()
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
+def sealed_class_negative_as_text() -> str:
+    return (Path("tests") / "vectors" / "sealed_class_negative.kt").read_text()
+
+
+@pytest.fixture(scope="module")
 def file_as_text() -> str:
     return (Path("tests") / "vectors" / "file.kt").read_text()
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def config_as_path() -> Path:
     return Path("tests") / "vectors" / "config.toml"
