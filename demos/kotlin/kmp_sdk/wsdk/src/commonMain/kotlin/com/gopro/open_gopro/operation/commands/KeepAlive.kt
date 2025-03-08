07344b9052d8b127bdf83ca8874c9835660bd3a0
Tim Camise <tcamise@gopro.com>
2025-03-07 16:20:45 -0800

    Fix keep alive

--- demos/kotlin/kmp_sdk/wsdk/src/commonMain/kotlin/com/gopro/open_gopro/operation/commands/KeepAlive.kt
+++ demos/kotlin/kmp_sdk/wsdk/src/commonMain/kotlin/com/gopro/open_gopro/operation/commands/KeepAlive.kt
@@ -5,12 +5,11 @@ package com.gopro.open_gopro.operations.commands
 
 import com.gopro.open_gopro.domain.api.BaseOperation
 import com.gopro.open_gopro.domain.communicator.BleCommunicator
-import com.gopro.open_gopro.operations.Led
 import com.gopro.open_gopro.operations.SettingId
 
 internal class KeepAlive : BaseOperation<Unit>("Keep Alive") {
 
   @OptIn(ExperimentalUnsignedTypes::class)
   override suspend fun execute(communicator: BleCommunicator): Result<Unit> =
-      communicator.executeSetting(SettingId.LED, ubyteArrayOf(Led.KEEP_ALIVE.value)).map {}
+      communicator.executeSetting(SettingId.LED, ubyteArrayOf(66U)).map {}
 }
