b0b1648a713343ce70271171f8ece68eb0e8d16c
Tim Camise <tcamise@gopro.com>
2025-03-20 10:55:36 -0700

    Fix kotlin formatting

--- demos/kotlin/kmp_sdk/wsdk/src/commonTest/kotlin/com/gopro/open_gopro/network/TestBleAdvertisementParsing.kt
+++ demos/kotlin/kmp_sdk/wsdk/src/commonTest/kotlin/com/gopro/open_gopro/network/TestBleAdvertisementParsing.kt
@@ -4,102 +4,94 @@
 package com.gopro.open_gopro.network
 
 import com.gopro.open_gopro.domain.communicator.bleCommunicator.GpBleAdvertisement
-import vectors.advResponseManufData
-import vectors.scanResponseFea6ServiceData
 import kotlin.test.Test
 import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
+import vectors.advResponseManufData
+import vectors.scanResponseFea6ServiceData
 
 @OptIn(ExperimentalUnsignedTypes::class)
 class TestBleAdvertisementParsing {
-    @Test
-    fun `parse advertisement`() {
-        // GIVEN
-        val builder = GpBleAdvertisement.Builder()
-        val serviceData = scanResponseFea6ServiceData.copyOf()
-        val manuData = advResponseManufData.copyOf()
+  @Test
+  fun `parse advertisement`() {
+    // GIVEN
+    val builder = GpBleAdvertisement.Builder()
+    val serviceData = scanResponseFea6ServiceData.copyOf()
+    val manuData = advResponseManufData.copyOf()
 
-        // WHEN
-        val gpAdvertisement =
-            builder
-                .name("name")
-                .serviceData(serviceData)
-                .manufacturerData(manuData)
-                .build()
+    // WHEN
+    val gpAdvertisement =
+        builder.name("name").serviceData(serviceData).manufacturerData(manuData).build()
 
-        // THEN
-        with(gpAdvertisement) {
-            assertEquals(name, "name")
-            assertEquals(schemaVersion, 2)
-            assertTrue { processorState }
-            assertFalse { wifiApState }
-            assertFalse { peripheralPairingState }
-            assertFalse { centralRoleEnabled }
-            assertFalse { isNewMediaAvailable }
-            assertEquals(65, cameraId)
-            assertTrue { supportsCnc }
-            assertTrue { supportsBleMetadata }
-            assertFalse { supportsWidebandAudio }
-            assertFalse { supportsConcurrentMasterSlave }
-            assertFalse { supportsOnboarding }
-            assertTrue { supportsNewMediaAvailable }
-            assertContentEquals(ubyteArrayOf(0x99U, 0x64U, 0x26U, 0x61U, 0x21U, 0xe0U), idHash)
-            assertFalse { isMediaUploadAvailable }
-            assertFalse { isMediaUploadNewMediaAvailable }
-            assertFalse { isMediaUploadBatteryOk }
-            assertFalse { isMediaUploadSdCardOk }
-            assertFalse { isMediaUploadBusy }
-            assertFalse { isMediaUploadPaused }
-            assertContentEquals(ubyteArrayOf(0x47U, 0x3bU, 0x28U, 0x2dU), apMacAddress)
-            assertEquals("0053", partialSerialNumber)
-            assertEquals(serialNumber, "C353XXXXXX0053")
-        }
+    // THEN
+    with(gpAdvertisement) {
+      assertEquals(name, "name")
+      assertEquals(schemaVersion, 2)
+      assertTrue { processorState }
+      assertFalse { wifiApState }
+      assertFalse { peripheralPairingState }
+      assertFalse { centralRoleEnabled }
+      assertFalse { isNewMediaAvailable }
+      assertEquals(65, cameraId)
+      assertTrue { supportsCnc }
+      assertTrue { supportsBleMetadata }
+      assertFalse { supportsWidebandAudio }
+      assertFalse { supportsConcurrentMasterSlave }
+      assertFalse { supportsOnboarding }
+      assertTrue { supportsNewMediaAvailable }
+      assertContentEquals(ubyteArrayOf(0x99U, 0x64U, 0x26U, 0x61U, 0x21U, 0xe0U), idHash)
+      assertFalse { isMediaUploadAvailable }
+      assertFalse { isMediaUploadNewMediaAvailable }
+      assertFalse { isMediaUploadBatteryOk }
+      assertFalse { isMediaUploadSdCardOk }
+      assertFalse { isMediaUploadBusy }
+      assertFalse { isMediaUploadPaused }
+      assertContentEquals(ubyteArrayOf(0x47U, 0x3bU, 0x28U, 0x2dU), apMacAddress)
+      assertEquals("0053", partialSerialNumber)
+      assertEquals(serialNumber, "C353XXXXXX0053")
     }
+  }
 
-    @Test
-    fun `parse advertisement invalid model id`() {
-        // GIVEN
-        val builder = GpBleAdvertisement.Builder()
-        val serviceData = scanResponseFea6ServiceData.copyOf()
-        val manuData = advResponseManufData.copyOf()
-        manuData[2] = 0xFF.toByte()
+  @Test
+  fun `parse advertisement invalid model id`() {
+    // GIVEN
+    val builder = GpBleAdvertisement.Builder()
+    val serviceData = scanResponseFea6ServiceData.copyOf()
+    val manuData = advResponseManufData.copyOf()
+    manuData[2] = 0xFF.toByte()
 
-        // WHEN
-        val gpAdvertisement =
-            builder
-                .name("name")
-                .serviceData(serviceData)
-                .manufacturerData(manuData)
-                .build()
+    // WHEN
+    val gpAdvertisement =
+        builder.name("name").serviceData(serviceData).manufacturerData(manuData).build()
 
-        // THEN
-        with(gpAdvertisement) {
-            assertEquals(name, "name")
-            assertEquals(schemaVersion, 2)
-            assertTrue { processorState }
-            assertFalse { wifiApState }
-            assertFalse { peripheralPairingState }
-            assertFalse { centralRoleEnabled }
-            assertFalse { isNewMediaAvailable }
-            assertEquals(-1, cameraId)
-            assertTrue { supportsCnc }
-            assertTrue { supportsBleMetadata }
-            assertFalse { supportsWidebandAudio }
-            assertFalse { supportsConcurrentMasterSlave }
-            assertFalse { supportsOnboarding }
-            assertTrue { supportsNewMediaAvailable }
-            assertContentEquals(ubyteArrayOf(0x99U, 0x64U, 0x26U, 0x61U, 0x21U, 0xe0U), idHash)
-            assertFalse { isMediaUploadAvailable }
-            assertFalse { isMediaUploadNewMediaAvailable }
-            assertFalse { isMediaUploadBatteryOk }
-            assertFalse { isMediaUploadSdCardOk }
-            assertFalse { isMediaUploadBusy }
-            assertFalse { isMediaUploadPaused }
-            assertContentEquals(ubyteArrayOf(0x47U, 0x3bU, 0x28U, 0x2dU), apMacAddress)
-            assertEquals("0053", partialSerialNumber)
-            assertEquals(serialNumber, "XXXXXXXXXX0053")
-        }
+    // THEN
+    with(gpAdvertisement) {
+      assertEquals(name, "name")
+      assertEquals(schemaVersion, 2)
+      assertTrue { processorState }
+      assertFalse { wifiApState }
+      assertFalse { peripheralPairingState }
+      assertFalse { centralRoleEnabled }
+      assertFalse { isNewMediaAvailable }
+      assertEquals(-1, cameraId)
+      assertTrue { supportsCnc }
+      assertTrue { supportsBleMetadata }
+      assertFalse { supportsWidebandAudio }
+      assertFalse { supportsConcurrentMasterSlave }
+      assertFalse { supportsOnboarding }
+      assertTrue { supportsNewMediaAvailable }
+      assertContentEquals(ubyteArrayOf(0x99U, 0x64U, 0x26U, 0x61U, 0x21U, 0xe0U), idHash)
+      assertFalse { isMediaUploadAvailable }
+      assertFalse { isMediaUploadNewMediaAvailable }
+      assertFalse { isMediaUploadBatteryOk }
+      assertFalse { isMediaUploadSdCardOk }
+      assertFalse { isMediaUploadBusy }
+      assertFalse { isMediaUploadPaused }
+      assertContentEquals(ubyteArrayOf(0x47U, 0x3bU, 0x28U, 0x2dU), apMacAddress)
+      assertEquals("0053", partialSerialNumber)
+      assertEquals(serialNumber, "XXXXXXXXXX0053")
     }
+  }
 }
