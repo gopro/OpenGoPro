dfd7714db81c11e03115a1a3b03d6002cb9e2466
Tim Camise <tcamise@gopro.com>
2025-04-22 09:32:53 -0700

    Add scheduled capture demo

---
+++ demos/python/sdk_wireless_camera_control/open_gopro/demos/scheduled_capture.py
@@ -0,0 +1,78 @@
+# photo.py/Open GoPro, Version 2.0 (C) Copyright 2021 GoPro, Inc. (http://gopro.com/OpenGoPro).
+# This copyright was auto-generated on Wed, Sep  1, 2021  5:05:45 PM
+
+"""Entrypoint for taking a picture demo."""
+
+import argparse
+import asyncio
+from pathlib import Path
+from datetime import timedelta
+
+from rich.console import Console
+
+from open_gopro import WiredGoPro, WirelessGoPro, proto
+from open_gopro.gopro_base import GoProBase
+from open_gopro.logger import setup_logging
+from open_gopro.models.general import ScheduledCapture
+from open_gopro.util import add_cli_args_and_parse, get_current_dst_aware_time
+
+console = Console()
+
+
+async def main(args: argparse.Namespace) -> None:
+    logger = setup_logging(__name__, args.log)
+
+    gopro: GoProBase | None = None
+
+    try:
+        async with WirelessGoPro(args.identifier, enable_wifi=False) as gopro:
+            assert gopro
+            # Enter video mode
+            assert (await gopro.ble_command.load_preset_group(group=proto.EnumPresetGroup.PRESET_GROUP_ID_VIDEO)).ok
+
+            # Set the camera's datetime
+            now, tz_offset, is_dst = get_current_dst_aware_time()
+            assert (await gopro.ble_command.set_date_time_tz_dst(date_time=now, tz_offset=tz_offset, is_dst=is_dst)).ok
+
+            # Get the current scheduled capture value
+            assert (await gopro.ble_setting.scheduled_capture.get_value()).ok
+
+            # Set the video duration
+            TODO
+            # Configure scheduled capture for one minute in the future
+            capture_time = now + timedelta(minutes=1)
+            assert (
+                await gopro.ble_setting.scheduled_capture.set(ScheduledCapture.from_datetime(capture_time, True))
+            ).ok
+
+    except Exception as e:  # pylint: disable = broad-except
+        logger.error(repr(e))
+
+    if gopro:
+        await gopro.close()
+
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Connect to a GoPro camera, take a photo, then download it.")
+    parser.add_argument(
+        "--output",
+        type=Path,
+        help="Where to write the photo to. If not set, write to 'photo.jpg'",
+        default=Path("photo.jpg"),
+    )
+    parser.add_argument(
+        "--wired",
+        action="store_true",
+        help="Set to use wired (USB) instead of wireless (BLE / WIFI) interface",
+    )
+
+    return add_cli_args_and_parse(parser)
+
+
+# Needed for poetry scripts defined in pyproject.toml
+def entrypoint() -> None:
+    asyncio.run(main(parse_arguments()))
+
+
+if __name__ == "__main__":
+    entrypoint()
