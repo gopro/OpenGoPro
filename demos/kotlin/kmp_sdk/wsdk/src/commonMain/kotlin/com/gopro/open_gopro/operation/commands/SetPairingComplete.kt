712e57ff9788325e5057ccc4c5cadddf94b8b63b
Tim Camise <tcamise@gopro.com>
2025-06-06 10:35:37 -0700

    Kotlin SDK: Always send pairing complete after opening BLE

---
+++ demos/kotlin/kmp_sdk/wsdk/src/commonMain/kotlin/com/gopro/open_gopro/operation/commands/SetPairingComplete.kt
@@ -0,0 +1,28 @@
+package com.gopro.open_gopro.operations.commands
+
+import com.gopro.open_gopro.domain.api.BaseOperation
+import com.gopro.open_gopro.domain.communicator.BleCommunicator
+import com.gopro.open_gopro.domain.communicator.bleCommunicator.ResponseId
+import com.gopro.open_gopro.entity.communicator.ActionId
+import com.gopro.open_gopro.entity.communicator.FeatureId
+import com.gopro.open_gopro.entity.network.ble.GpUuid
+import com.gopro.open_gopro.operations.EnumPairingFinishState
+import com.gopro.open_gopro.operations.RequestPairingFinish
+import com.gopro.open_gopro.util.extensions.mapFromGenericProtoResponseToResult
+import pbandk.encodeToByteArray
+
+internal class SetPairingComplete : BaseOperation<Unit>("Set Pairing Complete") {
+
+  override suspend fun execute(communicator: BleCommunicator): Result<Unit> {
+    val request =
+        RequestPairingFinish(result = EnumPairingFinishState.SUCCESS, phoneName = "phoneName")
+    return communicator
+        .executeProtobufCommand(
+            FeatureId.WIRELESS_MANAGEMENT,
+            ActionId.REQUEST_PAIRING_STATE,
+            request.encodeToByteArray(),
+            ResponseId.Protobuf(FeatureId.WIRELESS_MANAGEMENT, ActionId.REQUEST_PAIRING_STATE_RSP),
+            GpUuid.CM_NET_MGMT_COMM)
+        .mapFromGenericProtoResponseToResult()
+  }
+}
