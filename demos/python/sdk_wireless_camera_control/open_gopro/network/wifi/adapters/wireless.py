f880eb4398fe72961e00714b2eafa0f263bab50f
James Crosby <james@coggle.it>
2025-08-12 17:33:04 +0100

    lint

--- demos/python/sdk_wireless_camera_control/open_gopro/network/wifi/adapters/wireless.py
+++ demos/python/sdk_wireless_camera_control/open_gopro/network/wifi/adapters/wireless.py
@@ -798,8 +798,8 @@ class NetworksetupWireless(WifiController):
             # For current MacOs versions or if above failed
             if match := re.search(r"\n\s+SSID : ([\x20-\x7E]{1,32})", cmd(f"ipconfig getsummary {self.interface}")):
                 ssid = match.group(1)
-        if ssid == '<redacted>':
-            raise RuntimeError('current Wi-Fi network is redacted')
+        if ssid == "<redacted>":
+            raise RuntimeError("current Wi-Fi network is redacted")
 
         return (ssid, SsidState.CONNECTED) if ssid else (None, SsidState.DISCONNECTED)
 
